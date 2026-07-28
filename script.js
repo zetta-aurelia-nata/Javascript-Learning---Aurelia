02c2fffd09ca8815887d09b4f5e76a15ad4169d0
Aurelia Davine Putri Nata <aurel.nata@zetta-intern.space>
2026-07-28 14:17:59 +0700

    your message

---
+++ script.js
@@ -0,0 +1,61 @@
+let firstFavoriteBook = "The first frost";
+const secondFavoriteBook = "King of Clash";
+
+firstFavoriteBook = "Twilight";
+const bookCollection = firstFavoriteBook + " & " + secondFavoriteBook;
+
+console.log("Book Collection:", bookCollection);
+
+const studentName = "Aurelia Davine Putri Nata";
+let age = 21;
+const isIntern = true;
+let address = "Makassar";
+let middleName = null;
+
+console.log("Nama : ", studentName);
+console.log("umur : ",age);
+console.log("sedang magang? : " ,isIntern);
+console.log("Alamat Tempat Tinggal: ", address);
+console.log("Nama Tengah: ",middleName);
+
+let dynamicValue = "Hello";
+console.log("Welcome text : ", dynamicValue);
+
+dynamicValue = 100;
+console.log(dynamicValue);
+
+dynamicValue = false;
+console.log(dynamicValue);
+
+const Hobbies = ["Dance", "Singing", "Watching movie"];
+
+console.log(Hobbies);
+
+const student = {
+  name: "Aurelia",
+  age: 21,
+  major: "Informatics",
+  isIntern: true
+};
+
+console.log(student);
+
+const books = [
+  {
+    title: "The first frost ",
+    author: "Aurel",
+    year: 2018
+  },
+  {
+    title: "King of Clash",
+    author: "RRone",
+    year: 2020
+  },
+  {
+    title: "Twilight",
+    author: "hai",
+    year: 2017
+  }
+];
+
+console.log(books);
