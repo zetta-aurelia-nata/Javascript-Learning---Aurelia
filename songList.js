dcad8ad74b978cbeb85e3bea26897a14494d4860
Aurelia Davine Putri Nata <aurel.nata@zetta-intern.space>
2026-08-05 13:26:27 +0700

    Create songList.js

---
+++ songList.js
@@ -0,0 +1,141 @@
+const songs = [
+    {
+        title: "Perfect",
+        artist: "Ed Sheeran",
+        genre: "Pop",
+        duration: 240
+    },
+    {
+        title: "Backburner",
+        artist: "NIKI",
+        genre: "Pop",
+        duration: 570
+    },
+    {
+        title: "Maestro",
+        artist: "Seventeen",
+        genre: "Kpop",
+        duration: 240
+    },
+    {
+        title: "London Boy",
+        artist: "Taylor Swift",
+        genre: "Pop",
+        duration: 820
+    },
+    {
+        title: "Love Story",
+        artist: "Taylor Swift",
+        genre: "Country Pop",
+        duration: 235
+    },
+    {
+        title: "Jatuh Suka",
+        artist: "Raisa",
+        genre: "Indo Pop",
+        duration: 503
+    },
+    {
+        title: "Ocean",
+        artist: "Niki",
+        genre: "Pop",
+        duration: 760
+    },
+];
+
+function displaySongs() {
+    return songs.map(({ title, artist, genre, duration }) => ({
+        title,
+        artist,
+        genre,
+        duration: duration + " seconds"
+    }));
+}
+
+function groupSongsByArtist() {
+    return songs.reduce((groupedSongs, song) => {
+        if (!groupedSongs[song.artist]) {
+            groupedSongs[song.artist] = [];
+        }
+        groupedSongs[song.artist].push(song);
+        return groupedSongs;
+    }, {});
+}
+
+function groupSongsByGenre() {
+    return songs.reduce((groupedSongs, song) => {
+        if (!groupedSongs[song.genre]) {
+            groupedSongs[song.genre] = [];
+        }
+        groupedSongs[song.genre].push(song);
+        return groupedSongs;
+    }, {});
+}
+
+function groupSongsLessThanOneHour() {
+    let totalDuration = 0;
+
+    return songs.filter(song => {
+        if (totalDuration + song.duration < 3600) {
+            totalDuration += song.duration;
+            return true;
+        }
+        return false;
+    });
+}
+
+function displayGroupByArtist() {
+    console.log("\n========================== GROUP BY ARTIST ==========================");
+
+    const groupedSongs = groupSongsByArtist();
+
+    Object.entries(groupedSongs).forEach(([artist, songs]) => {
+        console.log("Artist:", artist);
+
+        songs.forEach(({ title }) => {
+            console.log("- " + title);
+        });
+
+        console.log("");
+    });
+}
+
+function displayGroupByGenre() {
+    console.log("\n========================== GROUP BY GENRE ==========================");
+
+    const groupedSongs = groupSongsByGenre();
+
+    Object.entries(groupedSongs).forEach(([genre, songs]) => {
+        console.log("Genre:", genre);
+
+        songs.forEach(({ title }) => {
+            console.log("- " + title);
+        });
+
+        console.log("");
+    });
+}
+
+function displayGroupLessThanOneHour() {
+    console.log("\n========================== PLAYLIST LESS THAN 1 HOUR ==========================");
+
+    const groupedSongs = groupSongsLessThanOneHour();
+
+    groupedSongs.forEach(({ title, duration }) => {
+        console.log(title + " | Duration: " + duration + " seconds");
+    });
+
+    const totalDuration = groupedSongs.reduce(
+        (total, song) => total + song.duration, 0);
+
+    console.log("\nTotal Duration:", totalDuration, "seconds");
+    console.log("Less Than One Hour:", totalDuration < 3600);
+}
+
+const songListArray = displaySongs();
+console.log("\n========================== SONG LIST ARRAY ==========================");
+console.log(songListArray);
+
+displayGroupByArtist();
+displayGroupByGenre();
+displayGroupLessThanOneHour();
